b7e0c7a4456169052a35008706aed7cbcd64bd46
akk2000 <7.aungkaungkhant@gmail.com>
2025-11-22 20:00:01 +0630

    adding detail for each items

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
 let list = ""; 
 let tableRows = document.getElementById("table_rows");
+let cardBody = document.getElementById("card_body")
 
 fetch("https://fakestoreapi.com/products")
 .then(response => response.json())
 .then(result => {
     for(i =0; i < result.length; i++){
-        list += `<tr>
+        list += `<tr onclick="showDetail(${result[i].id})">
                     <td>${result[i].id}</td>
-                    <td>${result[i].category}</td>
-                    <td>${result[i].price}</td>
+                    <td>${result[i].title}</td>
+                    <td>${result[i].price}$</td>
                     <td><img src="${result[i].image}" class="api_images"/></td>
-                </tr> `;
+                </tr> `;        
         tableRows.innerHTML = list;
     }
-    
 });
+
+function showDetail(id){
+    console.log(id);
+    fetch("https://fakestoreapi.com/products/" + id)
+    .then(response => response.json())
+    .then(data => {
+        cardBody.innerHTML = `   
+        <img src="${data.image}">    
+        <div class="card-data">      
+            <h4>${data.title}</h4>
+            <h6>${data.description} </h6>     
+        </div>
+        `;
+    });
+    
+
+}
 
         
